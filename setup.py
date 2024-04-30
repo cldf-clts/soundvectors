b4667ff3ea5555544789120b28c02c3f2b4dbba9
Robert Forkel <xrotwang@googlemail.com>
2024-04-30 08:15:05 +0200

    updated package scaffolding

--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='soundvectors',
-    version='1.0.dev0',
-    license='MIT',
-    description='Vectorizing Speech Sounds in Phonetic Transcription',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    author='Arne Rubehn',
-    author_email='arne.rubehn@uni-passau.de',
-    url='https://github.com/calc-project/clts2vec',
-    keywords='speech sounds, feature vectors, CLTS',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    py_modules=['clts2vec'],
-    include_package_data=True,
-    zip_safe=False,
-    platforms='any',
-    python_requires='>=3.6',
-    install_requires=[],
-    extras_require={
-        'dev': ['pyclts', 'cltoolkit', 'scikit-learn', 'seaborn', 'numpy', 'matplotlib', 'lingpy'],
-        'test': [
-            "linse>=0.1.0",
-            'pytest>=4.3',
-            'pytest-cov',
-            'coverage>=4.2',
-        ],
-    },
-)
+
+setup()
